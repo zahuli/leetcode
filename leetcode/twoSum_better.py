25214f9843e376ab72bdf4de27c944306151eadf
Nikola Smiljanic <nikola.smiljanic@codetiq.io>
2023-04-21 14:36:50 +0200

    renamed a file and comments

--- leetcode/twoSum_better.py
+++ leetcode/twoSum_better.py
@@ -5,10 +5,11 @@ from typing import List
 def twoSum(nums: List[int], target: int) -> List[int]:
     d = {}
     for index, val in enumerate(nums):
-        rem = target - val  # oduzima trenutni broj od targeta i smesta u dictionary
-        if rem in d:  # posle proverava da li je se sledeci vec nalazi u dic
+        rem = target - val  # sunstracts current number from target and puts it to a dictionary
+        if rem in d:  # after, it checks if the next one is already in dictionary
             return [d[rem], index]
-        d[val] = index  # ako se razlika ne nalazi u dic, onda ga smesta
+        # if the difference is NOT in the dictionary, than it puts it in dictionary
+        d[val] = index
 
 
 print(twoSum([2, 7, 11, 15], 9))
