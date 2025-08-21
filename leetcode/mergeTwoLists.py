8b050a1aea8f6cabd5e9c901101d9d4e069599e1
Nikola Smiljanic <nikola.smiljanic@codetiq.io>
2025-08-21 15:33:12 +0200

    added mergeTwoLists.py

--- leetcode/mergeTwoLists.py
+++ leetcode/mergeTwoLists.py
@@ -1,44 +1,52 @@
 # 21. Merge Two Sorted Lists
 # https://leetcode.com/problems/merge-two-sorted-lists/
 
-class Node:
-    def __init__(self, data=0):
-        self.data = data
-        self.next = None
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
-class List:
-    def __init__(self):
-        self.head = None
+def build_linked_list(arr):
+    if not arr:  # empty array
+        return None
+    head = ListNode(arr[0])  # create head node
+    current = head
+    for val in arr[1:]:
+        current.next = ListNode(val)
+        current = current.next
 
-    def listprint(self):
-        printval = self.head
-        while printval is not None:
-            print(printval.data)
-            printval = printval.next
-
-
-def createList(l):
-    if l == []:
-        return List()
-
-    HeadVal = List()
-    HeadVal.head = Node(l[0])
-    if len(l) > 1:
-        for item in range(1, len(l)):
-            currentNode = Node(l[item])
-            if item == 1:
-                HeadVal.head.next = currentNode
-                prev = currentNode
-            else:
-                prev.next = currentNode
-                prev = currentNode
-
-    return HeadVal
+    return head
 
 
-l = [1, 2, 5, 6, 7, 10, 11, 14, 20, 34, 56]
+def mergeTwoLists(list1, list2):
+    current = head = ListNode()  # dummy node
+    while list1 and list2:
+        if list1.val < list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
 
-lista = createList(l)
-lista.listprint()
-print(type(lista))
+    if list1 or list2:
+        current.next = list1 if list1 else list2
+
+    return head.next
+
+
+l1 = [1, 2, 4]
+l2 = [1, 3, 4]
+list1 = build_linked_list(l1)
+list2 = build_linked_list(l2)
+
+l3 = mergeTwoLists(list1, list2)
+
+curr = l3
+while curr:
+    if curr.next:
+        print(curr.val, end=" -> ")
+    else:
+        print(curr.val)   # last element, no arrow
+    curr = curr.next
